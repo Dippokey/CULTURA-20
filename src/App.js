bea611b166f8131f29d54d4d6162f5b597c95c23
Mohammed Rifas S <rifas2475@outlook.com>
2020-01-29 00:22:43 +0530

    First section commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React, { Component} from 'react';
-import logo from './logo.jpeg';
+import React, { Component } from 'react';
+import logo from './Cultura20Logo3.png';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav} from 'react-bootstrap';
 
-import {
-  Route,
-  NavLink,
-  HashRouter
-} from "react-router-dom";
+
 
 class App extends Component {
   render() {
     return (
-      <h1></h1>
+      <>
+        <Navbar scrolling dark expand="md" fixed="top" expand="lg">
+          <Navbar.Brand href="#home"><img alt='' className="logo_size" src={logo}/></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="hello">
+              <Nav.Link href="#home"><span className="fields">Home</span></Nav.Link>
+              <Nav.Link href="#link"><span className="fields">Events</span></Nav.Link>
+              <Nav.Link href="#link"><span className="fields">Sponsors</span></Nav.Link>
+              <Nav.Link href="#link"><span className="fields">About Us</span></Nav.Link>
+              <Nav.Link href="#link"><span className="fields">Contact</span></Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div className="wrapper">
+          <a href="#" className="button">Register Now!</a>
+        </div>
+        </>
     );
   }
 }
