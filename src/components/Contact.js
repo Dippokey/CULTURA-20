2d04210d2a166c3d1f30baf7f1679db7f585633c
Shubham Goyal <shugo17cs@cmrit.ac.in>
2020-02-05 22:34:09 +0530

    final commit

---
+++ src/components/Contact.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-social/bootstrap-social.css';
+
+function Contact() {
+  return (
+    <div className="Contact">
+      <footer className="footer bg-dark text-white">
+        <div className="container">
+          <div className="row">
+            <div className="col-4 offset-1 col-sm-2">
+              <h5>Links</h5>
+              <ul className="list-unstyled">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+            <div className="col-7 col-sm-5">
+              <h5>Our Address</h5>
+              <address>
+                132, ITPL Main Rd, Brookefield, <br></br>
+                Sri Nivasa Reddy Layout, Kundalahalli,<br></br>
+                Bengaluru, Karnataka 560037<br></br>
+                <i className="fa fa-phone fa-lg"></i>: +91 96207 36801<br></br>
+                <i className="fa fa-phone fa-lg"></i>: +91 95387 87268<br></br>
+                <i className="fa fa-envelope fa-lg"></i>:
+                  <a href="mailto:shubshubham1@gmail.com">
+                  cultura@cmrit.ac.in
+                  </a>
+              </address>
+
+
+            </div>
+            <div className="col-12 col-sm-4 align-self-center">
+              <div className="text-center">
+                <a className="btn btn-social-icon btn-google" href="http://google.com/+"><i className="fa fa-google-plus"></i></a>
+                <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/profile.php?id="><i className="fa fa-facebook"></i></a>
+                <a className="btn btn-social-icon btn-linkedin" href="http://www.linkedin.com/in/"><i className="fa fa-linkedin"></i></a>
+                <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter"></i></a>
+                <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube"></i></a>
+                <a className="btn btn-social-icon" href="mailto:"><i className="fa fa-envelope-o"></i></a>
+              </div>
+            </div>
+          </div>
+          <div className="row justify-content-center">
+            <div className="col-auto">
+              <p>© Copyright CMR Institute of Tecnology</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+    </div>
+
+  );
+}
+
+export default Contact;
