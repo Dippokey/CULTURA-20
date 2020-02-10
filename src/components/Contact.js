b7b0f138e259f6b9fd8398990359cb1f97ba9b40
DIPNARAYAN ROY <dipnarayan.roy@gmail.com>
2020-02-10 13:06:34 +0530

    padding and event buttons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ function Contact() {
   return (
     <div className="Contact">
       <footer className="footer bg-dark text-white">
-        <div className="container">
+        <div className="container" id="foot">
           <div className="row">
             <div className="col-4 offset-1 col-sm-2">
               <h5>Links</h5>
